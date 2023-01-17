58777f3b4d35d0fd12f1239602eb67a7f5e60327
THIAGO VINICIUS <othiago.vss.20@gmail.com>
2023-01-17 12:43:11 -0300

    While adicionado
    Adicionado o While para escolha de continuação e removido temporariamente o áudio da rolagem do dado

--- Dado.py
+++ Dado.py
@@ -1,6 +1,10 @@
-import random
-import playsound
+from random import randint
 
-teste = int(input("Deseja rolar um dado de quantos lados?"))
-playsound.playsound("diceroll.mp3")
-print("A sua rolagem deu: {}".format(random.randint(1,teste)))
+escolha = str('')
+while escolha != "N":
+    print('-||-'*9)
+    print("-ROLAGEM DE DADOS-")
+    teste = int(input("Deseja rolar um dado de quantos lados?"))
+    print("A sua rolagem deu: {}".format(randint(1,teste)))
+    print('-||-'*9)
+    escolha = str(input("Deseja continuar [S/N]:")).upper()
